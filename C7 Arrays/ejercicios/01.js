71c191fba799b19e0348fc6682dfac6bdff5fd06
Oliverotalorap <oliverotalorapor10@gmail.com>
2024-07-04 15:31:50 -0500

    240704A C7-arrays-01

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -1,6 +1,9 @@
 function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
+
+  return array[0]
+
 }
 
 module.exports = devolverPrimerElemento;
